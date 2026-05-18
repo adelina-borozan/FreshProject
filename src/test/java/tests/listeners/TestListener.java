f5ee68a4b27d1c8409827965c07ed7a9986ae13e
Adelina Borozan <adelina.borozan24@gmail.com>
2026-05-18 16:38:17 +0300

    allure report and screenshots

---
+++ src/test/java/tests/listeners/TestListener.java
@@ -0,0 +1,35 @@
+package tests.listeners;
+
+import io.qameta.allure.Allure;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import tests.utils.ScreenshotUtils;
+import utilities.DriverFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TestListener extends TestListenerAdapter {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+
+        WebDriver driver = DriverFactory.getDriver();
+
+        Path screenshotPath = ScreenshotUtils.takeScreenshot(
+                driver,
+                result.getName()
+        );
+
+        if (screenshotPath != null) {
+            try (InputStream screenshot = Files.newInputStream(screenshotPath)) {
+                Allure.addAttachment(result.getName(), "image/png", screenshot, ".png");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

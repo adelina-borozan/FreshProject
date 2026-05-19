a4260ad82754c429b341be523bdf8be61d787c31
Adelina Borozan <adelina.borozan24@gmail.com>
2026-05-19 21:02:52 +0300

    add extent report

--- src/test/java/tests/utils/ExtentManager.java
+++ src/test/java/tests/utils/ExtentManager.java
@@ -4,6 +4,8 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ExtentManager {
@@ -14,8 +16,11 @@ public class ExtentManager {
 
         if (extent == null) {
 
+            Path reportsDirectory = Path.of("extent-reports");
+            createReportsDirectory(reportsDirectory);
+
             ExtentSparkReporter reporter =
-                    new ExtentSparkReporter(Path.of("extent-reports", "ExtentReport.html").toString());
+                    new ExtentSparkReporter(reportsDirectory.resolve("ExtentReport.html").toString());
             reporter.config().setDocumentTitle("FreshProject Test Report");
             reporter.config().setReportName("FreshProject Automation Results");
             reporter.config().setTheme(Theme.STANDARD);
@@ -27,5 +32,13 @@ public class ExtentManager {
         }
 
         return extent;
+    }
+
+    private static void createReportsDirectory(Path reportsDirectory) {
+        try {
+            Files.createDirectories(reportsDirectory);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create Extent report directory", e);
+        }
     }
 }
